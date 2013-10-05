5d9865720cf9c39bfea9d82f1a45c367aaa6cd5a
Emily <ejhoehne@gmail.com>
2013-10-05 13:25:56 -0600

    Works on 1-3

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-$(document).ready(function() {
+	$(document).ready(function() 
+{
 
-	function convert_value_to_string(value) {
-		if (value > 10) {
-			switch (value) {
+		var cardModel= Backbone.Model.extend
+		({
+			defaults: 
+			{
+				value: 0, 
+				suit: '', 
+				str_value: ''
+			}
+
+		}); 
+		var cardModelView= Backbone.View.extend
+		({
+			template: _.template($('#card-template').html()), 
+			initialize: function ()
+			{
+				this.render(); 
+			}, 
+			render:function()
+			{
+				this.$el.html(this.template(this.model.attributes));
+			}
+
+		});
+
+	function convert_value_to_string(value) 
+	{
+		if (value > 10 || value < 2) 
+		{
+			switch (value) 
+			{
 				case 11:
 				return 'Jack';
 				break;
@@ -12,6 +40,9 @@ $(document).ready(function() {
 				case 13:
 				return 'King';
 				break;
+				case 1:
+				return 'Ace';
+				break;
 			}
 		}
 		return value.toString();
@@ -19,10 +50,26 @@ $(document).ready(function() {
 
 	var deck = [];
 	var suits = ['hearts', 'diamonds', 'spades', 'clubs'];
-	for (var i = 0; i<suits.length; i++) {
+	for (var i = 0; i<suits.length; i++) 
+	{
 		var suit = suits[i];
-		for (var j = 0; j<13; j++) {
-			deck.push({value:j+1, suit: suit});
-		}
-	}
+		for (var j = 0; j<13; j++) 
+		{
+			var cardModelInstance= new cardModel 
+			({
+				value: j+1, 
+				suit: suit, 
+				str_value: convert_value_to_string(j+1)
+			}); 
+			var cardViewInstance= new cardModelView 
+			({
+				model: cardModel
+			});
+			cardViewInstance.render();
+			deck.push(cardModelInstance);
+		};
+	};
+	var deckShuffle= _.shuffle(deck); 
+	console.log(cardModelInstance);
+
 });
